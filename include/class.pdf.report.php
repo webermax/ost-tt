de89798889676a041913aa17387fe819d592f72b
Maximilian Weber <post@wbrmx.de>
2017-02-27 11:53:31 +0100

    Add Hardware list to ticket reports

--- include/class.pdf.report.php
+++ include/class.pdf.report.php
@@ -353,6 +353,20 @@ class Ticket2Report extends mPDF
             }
         }
         
+        // hardware
+        $this->SetFont('Arial', 'B');
+        $this->MultiCell(186, 7.15, 'Hardware', 0, 'L', 0);
+        
+        $this->SetFont('Arial', '');
+        $sql = 'SELECT * FROM `ost_ticket_hardware` WHERE `ticket_id` = ' . $ticket->getId();
+	$res = db_query($sql);
+	$i = 0;
+	while($row = db_fetch_array($res, MYSQL_ASSOC)) {
+		// $row['unit_cost']
+		$this->WriteCell(93, 7.15, $row['qty'] . ' x ' . $row['description'] . ': ' . $row['total_cost'] . ' €', 0, ($i % 2) ? 1 : 0, 'L');
+		$i++;
+	}
+        
         // stats
         $stats = $this->getTicketStats();
         
